1cdb8d1b43fbc0afdd31468473816937a9bbce4d
Guilherme Santana <guiguilsantana@gmail.com>
2024-02-13 19:16:04 -0300

    feat: create User repository

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.picpaysimplificado.domain.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
